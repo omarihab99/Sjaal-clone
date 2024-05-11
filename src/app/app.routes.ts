a2d2389d69cbf16b440f4fc700c9c0f162170e6f
yomna mahmoud <yomnaali152@gmail.com>
2024-05-12 01:10:38 +0300

    updates on productDetails

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ProductDetailsComponent } from './Components/product-details/product-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"product", component:ProductDetailsComponent}
+];
