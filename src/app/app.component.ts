1ac5e7703b8173fa3beb8cb1a78fd3b53543dfc6
Dell <nourraasshhraf@gmail.com>
2024-05-14 16:47:45 +0300

    add intial nav and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductDetailsComponent } from './Components/product-details/product-details.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FeedbackComponent } from './Components/feedback/feedback.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    HeaderComponent ,
+    FeedbackComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import { ProductDetailsComponent } from './Components/product-details/product-details.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { FeedbackComponent } from './Components/feedback/feedback.component';
 
 export const routes: Routes = [
-    {path:"product", component:ProductDetailsComponent}
+    {path:"product", component:ProductDetailsComponent} ,
+    {path:"header", component:HeaderComponent},
+    {path:"feedback", component:FeedbackComponent},
+    {path:"footer", component:FooterComponent}
 ];
