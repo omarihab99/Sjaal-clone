9390a0a0deacf73bef909a77c98817395eb8c9b0
Omar Ihab Mohamed | Ejada Systems Ltd. <omar.ihab.mohamed99@gmail.com>
2024-05-14 16:47:02 +0300

    Added sidebar, main and collections cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MainsectionComponent } from './Components/mainsection/mainsection.component';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { CategorySectionComponent } from './Components/category-section/category-section.component';
+import { HomeComponent } from './Components/home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainsectionComponent, SidebarComponent, CategorySectionComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: HomeComponent },
+    { path: "collections/:collectionName", component: HomeComponent },
+    { path: "collections", component: HomeComponent }
+];
