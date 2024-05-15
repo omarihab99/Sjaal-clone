a9743531b8edda152cddd166dd1bf826a73e411f
BasmalaMostafa <85370151+BasmalaMostafa@users.noreply.github.com>
2024-05-15 05:21:13 -0700

    addingLogictoProductPage

--- src/app/Components/product-card/product-card.component.ts
+++ src/app/Components/product-card/product-card.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
+import { ProductsService } from '../../Services/products.service';
+import { ProductDirective } from './Directives/product.directive';
+
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe,HttpClientModule,ProductDirective],
+  providers:[ProductsService],
   templateUrl: './product-card.component.html',
-  styleUrls:['./product-card.component.css']
+  styleUrls:['./product-card.component.css'],
 })
-export class ProductCardComponent {
+export class ProductCardComponent{
+@Input() product:any;
 
 }
--- src/app/Components/products-page/products-page.component.ts
+++ src/app/Components/products-page/products-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { DescriptionComponent } from '../description/description.component';
 import { FiltrationComponent } from '../filtration/filtration.component';
 import { ProductsComponent } from '../products/products.component';
+import { ProductsService } from '../../Services/products.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-products-page',
@@ -9,11 +11,39 @@ import { ProductsComponent } from '../products/products.component';
   imports: [
     DescriptionComponent,
     FiltrationComponent,
-    ProductsComponent
+    ProductsComponent,
+    HttpClientModule
+  ],
+  providers:[
+    ProductsService
   ],
   templateUrl: './products-page.component.html',
   styles:''
 })
 export class ProductsPageComponent {
+  constructor(private productService:ProductsService){}
 
+  mydata:any;
+  products:any;
+  length:any;
+  desc:any;
+  
+  ngOnInit(): void {
+    this.productService.getAllProducts().subscribe({
+      next:(data)=>{
+        this.mydata=data;
+        this.products=this.mydata[0]["products"];
+      //console.log(this.mydata[0]["products"][0].description);
+        this.length=this.products.length;
+        this.desc=this.mydata[0]["desc"];
+      },
+      error:(err)=>{
+        console.log(err);
+      },
+      complete:()=>{
+        console.log("completed");
+        
+      }
+    });
+  }
 }
