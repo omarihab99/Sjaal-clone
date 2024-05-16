ac5f9e0eaa78964b601d75e153f202162a1f8de5
yomna mahmoud <yomnaali152@gmail.com>
2024-05-16 23:50:27 +0300

    updates on checkout page

---
+++ src/app/Components/checkout-products/checkout-products.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './checkout-products.component.html',
+  styleUrl: './checkout-products.component.css'
+})
+export class CheckoutProductsComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ProductDetailsComponent } from './Components/product-details/product-details.component';
+import { CheckoutComponent } from './Components/checkout/checkout.component';
 
 export const routes: Routes = [
-    {path:"product/:id", component:ProductDetailsComponent}
+    {path:"product/:id", component:ProductDetailsComponent},
+    {path:"checkout", component:CheckoutComponent}
 ];
