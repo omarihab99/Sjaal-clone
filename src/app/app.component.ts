c15feeb80fefbfa365020e9874d83f4846eff1cc
BasmalaMostafa <85370151+BasmalaMostafa@users.noreply.github.com>
2024-05-16 07:52:06 -0700

    restoreImages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductDetailsComponent } from './Components/product-details/product-details.component';
 import { ProductsPageComponent } from './Components/products-page/products-page.component';
+import { CheckoutFormComponent } from './Components/checkout-form/checkout-form.component';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,8 @@ import { ProductsPageComponent } from './Components/products-page/products-page.
   imports: [
     RouterOutlet,
     ProductDetailsComponent,
-    ProductsPageComponent
+    ProductsPageComponent,
+    CheckoutFormComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
